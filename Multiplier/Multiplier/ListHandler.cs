c833d4901699396c4f06447d6a85fd7432c6424f
Americo <americo.h.pere@outlook.com>
2017-09-13 11:23:57 +0200

    Uppgift Onsdag 2017-09-13

---
+++ Multiplier/Multiplier/ListHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplier
+{
+    public class ListHandler
+    {
+        public List<string> List = new List<string>();
+
+        public void AddFiveStrings()
+        {
+            List.Add("Marcos");
+            List.Add("Gentri");
+            List.Add("Nicolas");
+            List.Add("Mohammed");
+            List.Add("Mattias");
+        }
+
+        public void RemoveThirdString()
+        {
+            List.RemoveAt(2);
+        }
+
+        public void Sort()
+        {
+            List.Sort();
+        }
+        
+    }
+
+}
